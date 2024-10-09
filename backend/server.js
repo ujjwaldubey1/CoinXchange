7a678c60331393825257cd7b1ecff8f577cac8a1
sumonelse <sumonelse01@gmail.com>
2024-10-09 15:07:10 +0530

    Setup DB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import { config } from "./src/config/config.js"
 import app from "./src/app.js"
+import connectDB from "./src/config/db.js"
 
 const startServer = async () => {
+    connectDB()
+
     const port = config.port || 5500
 
     app.listen(port, () => {
